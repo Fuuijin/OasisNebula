2a189afa0a765f489b6d29db347b34fa211ee7fe
Mattias Ross <mattias.ross@pm.me>
2026-03-04 14:08:49 +0100

    Intellisense fix

---
+++ Source/OasisNebula/Public/OasisNebulaGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OasisNebulaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OASISNEBULA_API AOasisNebulaGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
